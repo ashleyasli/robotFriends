ae2a697ed2af1c62714089c8cff120e36c35fc8e
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2018-11-13 16:01:58 -0500

    Add files via upload

---
+++ build/precache-manifest.f5f49d9cb01cb3d36ad5d178039ee28c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b99cf8c664f14778b69f",
+    "url": "/static/js/main.b99cf8c6.chunk.js"
+  },
+  {
+    "revision": "7805dd5bc070e16cb1b3",
+    "url": "/static/js/1.7805dd5b.chunk.js"
+  },
+  {
+    "revision": "b99cf8c664f14778b69f",
+    "url": "/static/css/main.95d2d284.chunk.css"
+  },
+  {
+    "revision": "7805dd5bc070e16cb1b3",
+    "url": "/static/css/1.81e5294e.chunk.css"
+  },
+  {
+    "revision": "9fc1149d979d32f31a6640e663a0d518",
+    "url": "/index.html"
+  }
+];
